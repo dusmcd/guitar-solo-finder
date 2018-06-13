86e45f398b82a4b2104e195d33dc736a64f4ffdc
Dustin McDowell <dustinmcdowell1989@gmail.com>
2018-06-13 17:08:23 -0500

    configure app

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,15 +3,16 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
+import './navbar.css'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <h1>BOILERMAKER</h1>
+  <div className="nav-bar">
+    <h1>Guitar Solo Finder</h1>
     <nav>
       {isLoggedIn ? (
         <div>
           {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
+          <Link to="/">Home</Link>
           <a href="#" onClick={handleClick}>
             Logout
           </a>
